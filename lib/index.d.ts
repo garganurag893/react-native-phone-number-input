f0af3df89e3a21edaefce8dee2af9fc5b9075deb
Android Hulk <abubakarrafi83@gmail.com>
2022-01-03 17:24:55 +0500

    placeHolderTextColor Bug Fixed
    Hey Anurag
    Can we please merge the changes of @justinschier asap
    Thanks

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -16,6 +16,7 @@ export interface PhoneInputProps {
   disabled?: boolean;
   disableArrowIcon?: boolean;
   placeholder?: string;
+  placeholderTextColor?:string;
   onChangeCountry?: (country: Country) => void;
   onChangeText?: (text: string) => void;
   onChangeFormattedText?: (text: string) => void;
